21df97dfa3e74ef270344c4ad2acdf168a04ff4c
Derek Jackson <d4jackson84@gmail.com>
2021-07-05 17:41:37 -0400

    routing get and post

--- src/routes.ts
+++ src/routes.ts
@@ -8,23 +8,27 @@ let specialtyPizzas: Pizza[] = [
     {
         name: "Carnivore Carbonara",
         price: 19,
-        toppings: ["Pepperoni", "Braised Mouse", "Yard Bird", "Groundhog Sausage", "Braised Bunny"],
+        toppings: ["Pepperoni", " Braised Mouse", " Yard Bird", " Groundhog Sausage", " Braised Bunny"],
         id: 1
     },
     {
         name: "Grass Gremlin Gourmet",
         price: 13,
-        toppings: ["Fresh-cut Grass", "Tall Grass", "Regurgitable White Sauce"],
+        toppings: ["Fresh-cut Grass", " Tall Grass", " Grass Clippings", " Regurgitable White Sauce"],
         id: 2
     },
     {
         name: "Floppy Fish Delight",
         price: 23,
-        toppings: ["Wild Caught Tuna", "Grouper", "Goldfish", "Backyard Koi"],
+        toppings: ["Fishtank Flouder", " Grouper", " Bowl-raised Goldfish", " Backyard Koi", " Shredded Tuna"],
         id: 3
     },
 ]
 
+// pizzas array
+let cart: Pizza[] = [];
+
+// INTERFACES
 // pizza object
 interface Pizza {
     name?: string,
@@ -33,28 +37,53 @@ interface Pizza {
     id: number
 }
 
+// review form
+interface Review {
+    name: string,
+    comments?: string,
+    rating: string
+}
+
 // track pizza IDs as added to cart
 let nextId: number = 4;
 
 // ROUTES
 // get
 routes.get("/", (req, res) => {
-    res.render("homepage")
+    res.render("homepage", {specialtyPizzas});
 })
 
 routes.get("/specialtyPizzas", (req, res) => {
-    let results: Pizza[] = specialtyPizzas.filter((pizza) => {
-        
+    // FIX THE RESULTS TYPE BELOW
+    let results: Pizza|undefined = specialtyPizzas.find((pizza) => {
+        console.log(pizza.id);
         return pizza.id === Number(req.query.id)
         
     })
     // render chosen pizza
-    res.render("specialty-pizzas", {
+    res.render("specialtyPizzas", {
         
         pizza: results
     })
     console.log(results);
+})
 
+routes.get("/review", (req, res) => {
+    res.render("review");
+})
+
+// "review/confirmation?"
+routes.post("/confirmation", (req, res) => {
+    const name: string = req.body.name ? String(req.body.name) : "";
+    const comments: string = req.body?.comments ? String(req.body?.comments) : "";
+    const rating: number = req.body.value ? Number(req.body.value) : 0;
+
+    const newReview: Review = {
+        name: name,
+        comments: comments,
+        rating: rating
+
+    }
 })
 
 
