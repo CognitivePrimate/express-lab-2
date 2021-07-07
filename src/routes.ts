0d1df5115b027c660f29317615edf4fc55926e0f
Derek Jackson <d4jackson84@gmail.com>
2021-07-06 20:50:21 -0400

    object array blues

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import {toppings} from "./toppings";
+import {Topping, toppingChoices} from "./toppings";
 
 const routes = express.Router();
 
@@ -8,21 +8,24 @@ const routes = express.Router();
 let specialtyPizzas: Pizza[] = [
     {
         name: "Carnivore Carbonara",
+        size: "large",
         price: 19,
-        toppings: ["Pepperoni", " Braised Mouse", " Yard Bird", " Groundhog Sausage", " Braised Bunny"],
-        id: 1
+        toppings: [{name: "Pepperoni", price: 0}, {name: " Braised Mouse", price: 0}, {name: " Yard Bird", price: 0}, {name: " Groundhog Sausage", price: 0}, {name: " Braised Bunny", price: 0}],
+        isGlutenFree: "No"
     },
     {
         name: "Grass Gremlin Gourmet",
+        size: "large",
         price: 13,
-        toppings: ["Fresh-cut Grass", " Tall Grass", " Grass Clippings", " Regurgitable White Sauce"],
-        id: 2
+        toppings: [{name: "Fresh-cut Grass", price: 0}, {name: " Tall Grass", price: 0}, {name: " Grass Clippings", price: 0}, {name: " Regurgitable White Sauce", price:0}],
+        isGlutenFree: "No"
     },
     {
         name: "Floppy Fish Delight",
+        size: "large",
         price: 23,
-        toppings: ["Fishtank Flouder", " Grouper", " Bowl-raised Goldfish", " Backyard Koi", " Shredded Tuna"],
-        id: 3
+        toppings: [{name: "Fishtank Flouder", price: 0}, {name: " Grouper", price: 0}, {name: " Bowl-raised Goldfish", price: 0}, {name: " Backyard Koi", price: 0}, {name: " Shredded Tuna", price: 0}],
+        isGlutenFree: "No"
     },
 ]
 
@@ -31,9 +34,11 @@ let specialtyPizzas: Pizza[] = [
 // pizza object
 interface Pizza {
     name?: string,
+    size: string,
     price: number,
-    toppings: string[],
-    id: number
+    toppings: Topping[],
+    isGlutenFree: string,
+    specialInstructions?: boolean
 }
 
 
@@ -55,17 +60,17 @@ routes.get("/", (req, res) => {
 })
 
 routes.get("/specialtyPizzas", (req, res) => {
-    // FIX THE RESULTS TYPE BELOW
+    // finds specialty pizza in array by name, as specified on homepage.hbs
     let results: Pizza|undefined = specialtyPizzas.find((pizza) => {
-        console.log(pizza.id);
-        return pizza.id === Number(req.query.id)
-        
+        // console.log(pizza.toppings.topping.name)
+        return pizza.name === String(req.query.name)
     })
     // render chosen pizza
     res.render("specialtyPizzas", {
         
         pizza: results
     })
+    
     console.log(results);
 })
 
@@ -74,15 +79,48 @@ routes.get("/review", (req, res) => {
 })
 
 routes.get("/custom", (req, res) => {
-    console.log(toppings);
     res.render("custom", {
-        topping: toppings
+        topping: toppingChoices
     });
 })
 
-// "review/confirmation?"
-routes.post("/reviewConfirmation", (req, res) => {
+// POST
+// renders new page based on submit action of custom pizza building
+routes.post("/customConfirmation", (req, res) => {
+    // pizza interface variables
+    const size: string = req.body.sizeInput ? String(req.body.sizeInput) : "";
+    const instructions: string = req.body.instructions ? String(req.body.instructions) : ""
+    const chosenToppings: any = req.body.toppings ? String(req.body.toppings) : [];
     console.log(req.body);
+    console.log(`req.body.toppings ${req.body.toppings}`);
+    // chosenToppings.push(req.body.topping);
+    console.log(`Chosen Toppings: ${chosenToppings}`);
+    const isGlutenFree: string = req.body.gluten ? "yes" : "no"
+    // pricing configuration variables
+    let sizePrice: number = size === "small" ? 7 : size === "medium" ? 10 : 12;
+    console.log(`sizeprice: ${sizePrice}`)
+    let toppingsMultiplyer: any = sizePrice === 7 ? (.50 * chosenToppings.length).toFixed(2) : sizePrice === 10 ? (1 * chosenToppings.length).toFixed(2) : (1.25 * chosenToppings.length).toFixed(2)
+    // console.log(`toppingsmultiplyer: ${toppingsMultiplyer}`);
+    // Price Formula based on above variables
+    const price: number = sizePrice + toppingsMultiplyer;
+    const results: Pizza = {
+        // name?: "name",
+        size: size,
+        price: price,
+        toppings: chosenToppings,
+        isGlutenFree: isGlutenFree,
+    }
+    console.log(results);
+
+    res.render("customConfirmation", {
+        Pizza: results
+    });
+
+
+})
+
+// renders new page based on submit action of review section
+routes.post("/reviewConfirmation", (req, res) => {
     const name: string = req.body.name ? String(req.body.name) : "";
     const comments: string = req.body.comments ? String(req.body.comments) : "";
     const rating: string = req.body.rating ? String(req.body.rating) : "";
--- src/toppings.ts
+++ src/toppings.ts
@@ -3,21 +3,69 @@
 
 
 // toppings array & variables
-let chosenToppings: Topping[] = [];
-let smallToppingsPrice: number = .50 * chosenToppings.length;
+// export let chosenToppings: Topping[] = [];
+// let smallToppingsPrice: number = .50 * chosenToppings.length;
 
-export const toppings: Topping[] = [
+export const toppingChoices: Topping[] = [
     {
         name: "Fishtank Flounder",
-        price: 2
+        price: 1
     },
     {
         name: "Braised Bunny",
-        price: 4
+        price: 1
     },
     {
         name: "Bowl-raised Goldfish",
-        price: 3
+        price: 1
+    },
+    {
+        name: "Groundhog Sausage",
+        price: 1
+    },
+    {
+        name: "Yard Bird",
+        price: 1
+    },
+    {
+        name: "Backyard Koi",
+        price: 1
+    },
+    {
+        name: "Sewer Rat",
+        price: 1
+    },
+    {
+        name: "Shredded Tuna",
+        price: 1
+    },
+    {
+        name: "Stolen Sandwich Bread",
+        price: 1
+    },
+    {
+        name: "Yogurt",
+        price: 1
+    },
+    {
+        name: "Too-slow Fly",
+        price: 1
+    },
+    {
+        name: "Floor Spider",
+        price: 1
+    },
+    {
+        name: "Grilled Possum Tail",
+        price: 1
+    },
+    {
+        name: "Catnip",
+        price: 1
+    },
+    {
+        name: "Extra Cheese",
+        price: 1
     }
 ];
 
